e9c51ce39535c4cc87cd4f28a15734645a10c2a4
Oleksandr Levak <sashavytvyckyj@gmail.com>
2020-11-01 16:11:54 +0200

    Creating Collectibles
    Creating Collectibles

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+	public float rotateSpeed = 2;
+	void Update()
+	{
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime * rotateSpeed);
+	}
+}
