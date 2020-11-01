e9d7fecdd3985a6d6844c82e2ce4ab040f3d5e6b
Oleksandr Levak <sashavytvyckyj@gmail.com>
2020-11-01 16:50:39 +0200

    Store the value of collected PickUps
    Store the value of collected PickUps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,42 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
 
-    private Rigidbody rb;
+	public float speed;
+	public TextMeshProUGUI countText;
+	public GameObject winTextObject;
 
-    private float movementX;
-    private float movementY;
+	private float movementX;
+	private float movementY;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+	private Rigidbody rb;
+	private int count;
 
-    private void OnMove(InputValue movementValue)
-    {
-        Vector2 movementVector = movementValue.Get<Vector2>();
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText();
+		winTextObject.SetActive(false);
+	}
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
-    }
+	void FixedUpdate()
+	{
+		Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+		rb.AddForce(movement * speed);
+	}
 
-    private void FixedUpdate()
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
 
-        rb.AddForce(movement * speed);
-    }
+	void OnMove(InputValue value)
+	{
+		Vector2 v = value.Get<Vector2>();
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("PickUp"))
-        {
-            other.gameObject.SetActive(false);
-        }
-    }
+		movementX = v.x;
+		movementY = v.y;
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+
+		if (count >= 12)
+		{
+			winTextObject.SetActive(true);
+		}
+	}
 }
